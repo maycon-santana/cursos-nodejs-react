4f58da74a80e26becf1e0cf03abf1e6d0b30dfd3
maycon-santana <maycon.santana@thstec.com.br>
2023-01-22 20:28:37 -0300

    Logando e criando o token

--- sistema-pizzaria/backend/src/services/users/AuthUserService.ts
+++ sistema-pizzaria/backend/src/services/users/AuthUserService.ts
@@ -1,5 +1,6 @@
-import prismaClient from "../../prisma"
-import { compare } from "bcryptjs"
+import prismaClient from '../../prisma'
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken' 
 
 interface AuthRequest{
     email: string
@@ -26,7 +27,24 @@ class AuthUserService{
             throw new Error("Usuário/Senha está incorreta")
         }
 
-        return { ok: true }
+        // Gerar um token JWT e devolver os dados do usuario como id, nome e email
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            },
+        )
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
