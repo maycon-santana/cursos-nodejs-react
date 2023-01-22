e82dcd366f405c5e15d572510a364ef6484748ff
maycon-santana <maycon.santana@thstec.com.br>
2023-01-22 15:09:15 -0300

    Cadastrando User

--- sistema-pizzaria/backend/src/services/users/CreateUserService.ts
+++ sistema-pizzaria/backend/src/services/users/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma"
+
 interface UserRequest{
     name: string
     email: string
@@ -6,9 +8,36 @@ interface UserRequest{
 
 class CreateUserService {
     async execute({ name, email, password }: UserRequest) {
-        console.log(name)
+        // verificar se ele enviou um e-mail
+        if(!email) {
+            throw new Error("E-mail incorreto")
+        }
 
-        return { name: name }
+        // verificar se esse e-mail já está cadastrado
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(userAlreadyExists) {
+            throw new Error("Usuário já cadastrado")
+        }
+
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password,
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user
     }
 }
 
